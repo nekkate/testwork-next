ff4396ab4ea863b019a35864d44796fbbac4ec49
nekkate <catherinenekrylova@yandex.ru>
2023-05-24 01:09:13 +0400

    Add dot

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["nekkate.github.io/testwork-next/"],
-  },
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
